fe31474604c1ec0f1488a6786b2c055f45e83bdf
JulianChavarr <jchavarro-2021b@corhuila.edu.co>
2025-05-01 22:02:06 -0500

    🔧 Actualiza la configuración CORS en los controladores Actividad y Agenda

--- AgendaManager/src/main/java/com/corhuila/AgendaManager/Controller/AgendaController.java
+++ AgendaManager/src/main/java/com/corhuila/AgendaManager/Controller/AgendaController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.corhuila.AgendaManager.Entity.Agenda;
 import com.corhuila.AgendaManager.IService.IAgendaService;
 
-@CrossOrigin(origins = "http://107.22.67.73:8080")
+@CrossOrigin(origins = "http://agendadb.s3-website-us-east-1.amazonaws.com")
 @RestController
 @RequestMapping("api/agenda")
 public class AgendaController extends ABaseController<Agenda, IAgendaService> {
